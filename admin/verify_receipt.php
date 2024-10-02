e50cfc8edf21116fce9748b7908e1e446ed0dde5
_whitetxt <54776821+whitetxt@users.noreply.github.com>
2024-10-02 14:28:13 +0100

    Changed some variables and removed unneeded CSS from scan.php

--- admin/verify_receipt.php
+++ admin/verify_receipt.php
@@ -48,102 +48,103 @@ if (($user->access_level != USER_PERMISSION_SCAN) && ($user->access_level != USE
 <script src="/voucher/script/alert.js"></script>
 <script src="/voucher/script/jsQR.js"></script>
 <script>
-    const video = document.createElement("video");
-    const canvasElement = document.getElementById("canvas");
-    const canvas = canvasElement.getContext("2d");
-    // var output = document.getElementById("output");
-    const modal = document.getElementById("confirmation_modal");
-    const modal_value = document.getElementById("value");
-    const modal_time = document.getElementById("time");
-    const verify_spin = document.getElementById("verify-spin");
+const video = document.createElement("video");
+const canvasElement = document.getElementById("canvas");
+const canvas = canvasElement.getContext("2d");
+// var output = document.getElementById("output");
+const modal = document.getElementById("confirmation_modal");
+const modal_value = document.getElementById("value");
+const modal_time = document.getElementById("time");
+const verify_spin = document.getElementById("verify-spin");
 
-    function startScan() {
-        canvasElement.classList.remove("hidden");
-        // Use facingMode: environment to attempt to get the front camera on phones
-        navigator.mediaDevices.getUserMedia({
-            video: {
-                facingMode: "environment"
-            }
-        }).then(function (stream) {
-            video.srcObject = stream;
-            video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
-            video.play();
-            requestAnimationFrame(tick);
+function startScan() {
+    canvasElement.classList.remove("hidden");
+    // Use facingMode: environment to attempt to get the front camera on phones
+    navigator.mediaDevices.getUserMedia({
+        video: {
+            facingMode: "environment"
+        }
+    }).then(function(stream) {
+        video.srcObject = stream;
+        video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+        video.play();
+        requestAnimationFrame(tick);
+    });
+}
+
+function drawLine(begin, end, color) {
+    canvas.beginPath();
+    canvas.moveTo(begin.x, begin.y);
+    canvas.lineTo(end.x, end.y);
+    canvas.lineWidth = 2;
+    canvas.strokeStyle = color;
+    canvas.stroke();
+}
+
+function tick() {
+    if (video.readyState === video.HAVE_ENOUGH_DATA) {
+        canvasElement.height = video.videoHeight;
+        canvasElement.width = video.videoWidth;
+        canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+        var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
+        var code = jsQR(imageData.data, imageData.width, imageData.height, {
+            inversionAttempts: "dontInvert",
         });
-    }
-
-    function drawLine(begin, end, color) {
-        canvas.beginPath();
-        canvas.moveTo(begin.x, begin.y);
-        canvas.lineTo(end.x, end.y);
-        canvas.lineWidth = 2;
-        canvas.strokeStyle = color;
-        canvas.stroke();
-    }
-
-    function tick() {
-        if (video.readyState === video.HAVE_ENOUGH_DATA) {
-            canvasElement.height = video.videoHeight;
-            canvasElement.width = video.videoWidth;
-            canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-            var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
-            var code = jsQR(imageData.data, imageData.width, imageData.height, {
-                inversionAttempts: "dontInvert",
-            });
-            if (code && video.paused === false) {
-                drawLine(code.location.topLeftCorner, code.location.topRightCorner, "#FF3B58");
-                drawLine(code.location.topRightCorner, code.location.bottomRightCorner, "#FF3B58");
-                drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF3B58");
-                drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF3B58");
-                try {
-                    data = JSON.parse(code.data);
-                    modal.classList.toggle("modal-open");
-                    verify_spin.classList.remove("hidden");
-                    modal_value.classList.add("hidden");
-                    modal_time.classList.add("hidden");
-                    modal_value.innerText = `Value: ${data.amount.toFixed(2)}`;
-                    modal_time.innerText = `Time: ${new Date(data.time * 1000).toLocaleString()}`;
-                    video.pause();
-                    fetch("/voucher/api/admin/verify.php", {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-                        },
-                        body: `id=${data.transactionid}&amount=${data.amount}&time=${data.time}`,
-                    }).then((response) => {
-                        response.json().then((data) => {
-                            if (data.status === "success") {
-                                if (data.valid === true) {
-                                    modal.querySelector("#verify").innerText = "Verified!";
-                                } else {
-                                    modal.querySelector("#verify").innerText =
-                                        `QR data different to server: ${data.message}`;
-                                }
+        if (code && video.paused === false) {
+            drawLine(code.location.topLeftCorner, code.location.topRightCorner, "#FF3B58");
+            drawLine(code.location.topRightCorner, code.location.bottomRightCorner, "#FF3B58");
+            drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF3B58");
+            drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF3B58");
+            try {
+                data = JSON.parse(code.data);
+                modal.classList.toggle("modal-open");
+                verify_spin.classList.remove("hidden");
+                modal_value.classList.add("hidden");
+                modal_time.classList.add("hidden");
+                modal_value.innerText = `Value: ${data.amount.toFixed(2)}`;
+                modal_time.innerText = `Time: ${new Date(data.time * 1000).toLocaleString()}`;
+                video.pause();
+                fetch("/voucher/api/admin/verify.php", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                    },
+                    body: `id=${data.transactionid}&amount=${data.amount}&time=${data.time}`,
+                }).then((response) => {
+                    response.json().then((data) => {
+                        if (data.status === "success") {
+                            if (data.valid === true) {
+                                modal.querySelector("#verify").innerText =
+                                    "QR Code data matches server!";
                             } else {
                                 modal.querySelector("#verify").innerText =
-                                    "An server-side error occured. Manual check is required.";
-                                modal_value.classList.remove("hidden");
-                                modal_time.classList.remove("hidden");
+                                    `QR data different to server: ${data.message}`;
                             }
-                            verify_spin.classList.add("hidden");
-                        });
+                        } else {
+                            modal.querySelector("#verify").innerText =
+                                "An server-side error occured. Manual check is required.";
+                            modal_value.classList.remove("hidden");
+                            modal_time.classList.remove("hidden");
+                        }
+                        verify_spin.classList.add("hidden");
                     });
-                } catch (error) {
-                    console.log("wuuh");
-                    console.error(error);
-                    modal.querySelector("#verify").innerText =
-                        "An client-side error occured. Manual check is required.";
-                    modal_value.classList.remove("hidden");
-                    modal_time.classList.remove("hidden");
-                }
-            } else {
-                // output.innerText = "";
+                });
+            } catch (error) {
+                console.log("wuuh");
+                console.error(error);
+                modal.querySelector("#verify").innerText =
+                    "An client-side error occured. Manual check is required.";
+                modal_value.classList.remove("hidden");
+                modal_time.classList.remove("hidden");
             }
+        } else {
+            // output.innerText = "";
         }
-        requestAnimationFrame(tick);
     }
+    requestAnimationFrame(tick);
+}
 
-    startScan();
+startScan();
 </script>
 
 </html>
--- scan.php
+++ scan.php
@@ -18,19 +18,6 @@ if (($user->access_level != USER_PERMISSION_SCAN) && ($user->access_level != USE
     <?php require(PREFAB_PATH . "/global/head.php"); ?>
     <title>Scan</title>
 </head>
-<style>
-    #canvas {
-        margin-top: 2rem;
-    }
-
-    label {
-        font-size: 1.25em;
-    }
-
-    a {
-        cursor: pointer;
-    }
-</style>
 
 <body>
     <?php require(PREFAB_PATH . "/nav/nav.php"); ?>
@@ -58,7 +45,7 @@ if (($user->access_level != USER_PERMISSION_SCAN) && ($user->access_level != USE
                 <form method="dialog">
                     <!-- if there is a button in form, it will close the modal -->
                     <button class="btn" id="modal_yes" onclick="spend()">Yes</button>
-                    <button class="btn">No</button>
+                    <button class="btn" onclick="modal.classList.toggle('modal-open');video.play()">No</button>
                 </form>
             </div>
         </div>
@@ -69,109 +56,121 @@ if (($user->access_level != USER_PERMISSION_SCAN) && ($user->access_level != USE
 <script src="/voucher/script/alert.js"></script>
 <script src="/voucher/script/jsQR.js"></script>
 <script>
-    const video = document.createElement("video");
-    const canvasElement = document.getElementById("canvas");
-    const canvas = canvasElement.getContext("2d");
-    // var output = document.getElementById("output");
-    const modal = document.getElementById("confirmation_modal");
-    const modal_title = document.getElementById("title");
-    const modal_info = document.getElementById("info");
-    const modal_yes = document.getElementById("modal_yes");
-    const modal_actions = document.getElementById("modal_actions");
-    var latest_data = null;
+const video = document.createElement("video");
+const canvasElement = document.getElementById("canvas");
+const canvas = canvasElement.getContext("2d");
+// var output = document.getElementById("output");
+const modal = document.getElementById("confirmation_modal");
+const modal_title = document.getElementById("title");
+const modal_info = document.getElementById("info");
+const modal_yes = document.getElementById("modal_yes");
+const modal_actions = document.getElementById("modal_actions");
+var latest_data = null;
 
-    function startScan() {
-        canvasElement.classList.remove("hidden");
-        // Use facingMode: environment to attempt to get the front camera on phones
-        navigator.mediaDevices.getUserMedia({
-            video: {
-                facingMode: "environment"
-            }
-        }).then(function (stream) {
-            video.srcObject = stream;
-            video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
-            video.play();
-            requestAnimationFrame(tick);
-        });
-    }
-
-    function drawLine(begin, end, color) {
-        canvas.beginPath();
-        canvas.moveTo(begin.x, begin.y);
-        canvas.lineTo(end.x, end.y);
-        canvas.lineWidth = 2;
-        canvas.strokeStyle = color;
-        canvas.stroke();
-    }
-
-    function tick() {
-        if (video.readyState === video.HAVE_ENOUGH_DATA) {
-            canvasElement.height = video.videoHeight;
-            canvasElement.width = video.videoWidth;
-            canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-            var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
-            var code = jsQR(imageData.data, imageData.width, imageData.height, {
-                inversionAttempts: "dontInvert",
-            });
-            if (code && video.paused === false) {
-                drawLine(code.location.topLeftCorner, code.location.topRightCorner, "#FF3B58");
-                drawLine(code.location.topRightCorner, code.location.bottomRightCorner, "#FF3B58");
-                drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF3B58");
-                drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF3B58");
-                try {
-                    data = JSON.parse(code.data);
-                    latest_data = data;
-                    var amount = parseFloat(document.getElementById("amount").value);
-                    modal.classList.toggle("modal-open");
-                    modal_title.innerText = "Confirm account charge";
-                    modal_info.innerText = `Do you want to charge £${amount.toFixed(2)} to ${data.username}?`;
-                    modal_actions.classList.remove("hidden");
-                    video.pause();
-                } catch (error) {
-                    console.log("wuuh");
-                    console.error(error);
-                    create_alert("Invalid QR Code.");
-                }
-            } else {
-                // output.innerText = "";
-            }
+function startScan() {
+    canvasElement.classList.remove("hidden");
+    // Use facingMode: environment to attempt to get the front camera on phones
+    navigator.mediaDevices.getUserMedia({
+        video: {
+            facingMode: "environment"
         }
+    }).then(function(stream) {
+        video.srcObject = stream;
+        video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+        video.play();
         requestAnimationFrame(tick);
-    }
+    });
+}
 
-    function spend() {
-        const username = latest_data.username;
-        const secret = latest_data.secret;
-        const amount = parseFloat(document.getElementById("amount").value)
-        modal_title.innerText = "Processing charge...";
-        modal_info.innerHTML = '<span class="loading loading-spinner loading-lg"></span>';
-        modal_actions.classList.add("hidden");
-        var urlencoded = new URLSearchParams();
-        urlencoded.append("username", username);
-        urlencoded.append("secret", secret);
-        urlencoded.append("amount", amount);
-        fetch("/voucher/api/charge_account.php", {
+function drawLine(begin, end, color) {
+    canvas.beginPath();
+    canvas.moveTo(begin.x, begin.y);
+    canvas.lineTo(end.x, end.y);
+    canvas.lineWidth = 2;
+    canvas.strokeStyle = color;
+    canvas.stroke();
+}
+
+var frames_since_alert = 0;
+
+function tick() {
+    if (video.readyState === video.HAVE_ENOUGH_DATA) {
+        frames_since_alert++;
+        canvasElement.height = video.videoHeight;
+        canvasElement.width = video.videoWidth;
+        canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+        var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
+        var code = jsQR(imageData.data, imageData.width, imageData.height, {
+            inversionAttempts: "dontInvert",
+        });
+        if (code && video.paused === false) {
+            if (isNaN(parseFloat(document.getElementById("amount").value))) {
+                requestAnimationFrame(tick);
+                return;
+            }
+            drawLine(code.location.topLeftCorner, code.location.topRightCorner, "#FF3B58");
+            drawLine(code.location.topRightCorner, code.location.bottomRightCorner, "#FF3B58");
+            drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF3B58");
+            drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF3B58");
+            try {
+                data = JSON.parse(code.data);
+                latest_data = data;
+                if (data.username === undefined || data.secret === undefined) {
+                    throw "Invalid QR Code.";
+                }
+                var amount = parseFloat(document.getElementById("amount").value);
+                modal.classList.toggle("modal-open");
+                modal_title.innerText = "Confirm account charge";
+                modal_info.innerText = `Do you want to charge £${amount.toFixed(2)} to ${data.username}?`;
+                modal_actions.classList.remove("hidden");
+                video.pause();
+            } catch (error) {
+                console.error(error);
+                if (frames_since_alert > 30) {
+                    create_alert("Invalid QR Code.");
+                    frames_since_alert = 0;
+                }
+            }
+        } else {
+            // output.innerText = "";
+        }
+    }
+    requestAnimationFrame(tick);
+}
+
+function spend() {
+    const username = latest_data.username;
+    const secret = latest_data.secret;
+    const amount = parseFloat(document.getElementById("amount").value)
+    modal_title.innerText = "Processing charge...";
+    modal_info.innerHTML = '<span class="loading loading-spinner loading-lg"></span>';
+    modal_actions.classList.add("hidden");
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("username", username);
+    urlencoded.append("secret", secret);
+    urlencoded.append("amount", amount);
+    fetch("/voucher/api/charge_account.php", {
             method: "POST",
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded",
             },
             body: urlencoded
         })
-            .then(response => response.json())
-            .then(data => {
-                if (data.success) {
-                    create_alert("Successfully charged account!");
-                    setTimeout(() => {
-                        window.location.href = "/voucher/index.php";
-                    }, 1000);
-                } else {
-                    create_alert(data.error);
-                }
-            }).catch(error => {
-                console.error("Error:", error);
-                create_alert("An error occurred. Please try again.");
-            });
-    }
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                create_alert("Successfully charged account!");
+                setTimeout(() => {
+                    window.location.href = "/voucher/index.php";
+                }, 1000);
+            } else {
+                create_alert(data.error);
+            }
+        }).catch(error => {
+            console.error("Error:", error);
+            create_alert("An error occurred. Please try again.");
+        });
+}
 </script>
 
 </html>
